baccf3d9c80a9ef11f4f990dab30f1a52bacb450
ke-sato <keta0616@icloud.com>
2023-08-13 16:13:13 +0900

    create_image_01

--- app/controllers/keyboards_controller.rb
+++ app/controllers/keyboards_controller.rb
@@ -32,6 +32,7 @@ class KeyboardsController < ApplicationController
   private
 
   def read(result)
+    image = result["mediumImageUrls"].first
     model = result["itemName"]
     brand = result["shopCode"]
     price = result["itemPrice"]
@@ -41,6 +42,7 @@ class KeyboardsController < ApplicationController
     layout = extract_layout_from_caption(caption)
     os = extract_os_from_caption(caption)
     {
+      image: image,
       model: model,
       brand: brand,
       price: price,
@@ -50,6 +52,34 @@ class KeyboardsController < ApplicationController
       layout: layout,
       switch: switch,
     }
+  end
+
+  def extract_size_from_caption(caption)
+    size_pattern = /(フルサイズ|75%|60%|110キー|108キー|110%)/
+    match = caption.match(size_pattern)
+    return match[0] if match
+    nil
+  end
+
+  def extract_os_from_caption(caption)
+    os_pattern = /(MacOS|macOS|Windows 10|Windows 11|Chrome OS)/i
+    matches = caption.scan(os_pattern)
+    return matches if matches.any?
+    nil
+  end
+
+  def extract_layout_from_caption(caption)
+    layout_pattern = /(日本語配列|英語配列|JIS配列|US配列|日本語レイアウト)/
+    match = caption.match(layout_pattern)
+    return match[0] if match
+    nil
+  end
+
+  def extract_switch_from_caption(caption)
+    switch_pattern = /(メンブレン|パンタグラフ|メカニカルキーボード|静電容量無接点)/
+    match = caption.match(switch_pattern)
+    return match[0] if match
+    nil
   end
 
   def keyboard_params
