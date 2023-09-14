265c3ee37c5040e98e62b0aa7ac9d0cd041b0eb1
ke-sato <keta0616@icloud.com>
2023-09-14 10:38:59 +0900

    edit_search_action

--- app/controllers/keyboards_controller.rb
+++ app/controllers/keyboards_controller.rb
@@ -17,11 +17,13 @@ class KeyboardsController < ApplicationController
     @keyboards = []
     @name = params[:keyword]
     if @name.present?
+      # APIにリクエストを送信
       results = RakutenWebService::Ichiba::Item.search(keyword: @name, hits: 30 )
 
+      # レスポンスを処理
       results.each do |result|
         keyboard_info = read(result)
-        name_without_brackets = remove_brackets(keyboard_info[:name]) # 商品名から【】及び【】内の文字列を削除
+        name_without_brackets = remove_brackets(keyboard_info[:name]) # 商品名からremove_brackets内の文字列を削除
         keyboard_info[:name] = name_without_brackets # 更新した商品名を設定
 
         keyboard = Keyboard.new(keyboard_info)
@@ -31,7 +33,7 @@ class KeyboardsController < ApplicationController
     @keyboards.each do |keyboard|
       unless Keyboard.all.include?(keyboard)
         if keyboard.save
-          flash.now[:danger] = '取得しました'
+          flash.now[:success] = '取得しました'
         end
       end
 
@@ -78,7 +80,7 @@ class KeyboardsController < ApplicationController
     text.gsub(/\【.*?\】|＼.*?／|[.*?]|\b(国内正規品|着日指定不可|&限定価格&ポイント2倍&クーポン |正規保証品|正規保証|12ヶ月安心保証|一年間品質保証|ギフト|お誕生日|★即納|誕生日プレゼント|人気ギフト|贈り物|敬老の日|1年間無償保証|2年間無償保証|3年間無償保証|★絶賛発売中|送料無料|新生活)\b/, "")
   end
 
-  # OS
+  # osカラムに保存する文字列(全て小文字に変換し配列にして保存)
   def create_os_from_caption(caption)
     os_pattern = /(MacOS|macOS|Windows|windows|Linux|linux|Chrome OS|iOS|Android)/i
     matches = caption.scan(os_pattern)
@@ -90,7 +92,7 @@ class KeyboardsController < ApplicationController
     nil
   end
 
-  # size
+  # sizeカラムに保存する文字列
   def create_size_from_name_and_caption(text)
     size_pattern = /(フルサイズ|テンキー付き|テンキーレス|テンキーなし|60%|70%|75%|80%|100%|110%|67キー|92キー|104 キー|106キー|107キー|108キー|109キー|110キー|111キー|112キー)/
     match = text.match(size_pattern)
@@ -98,15 +100,15 @@ class KeyboardsController < ApplicationController
     nil
   end
 
-  # switch
+  # switchカラムに保存する文字列
   def create_switch_from_caption(caption)
-    switch_pattern = /(メンブレン|パンタグラフ|メカニカルキーボード|静電容量無接点)/
+    switch_pattern = /(メンブレン|パンタグラフ|メカニカル|静電容量無接点)/
     match = caption.match(switch_pattern)
     return match[0] if match
     nil
   end
 
-  # layout
+  # layoutカラムに保存する文字列
   def create_layout_from_caption(caption)
     layout_pattern = /(日本語配列|英語配列|JIS配列|US配列|日本語レイアウト)/
     match = caption.match(layout_pattern)
