62ba5da81288fbd760176ba1e06d580845a04584
ke-sato <keta0616@icloud.com>
2023-10-01 10:25:01 +0900

    edit_autocomplete_color

--- app/models/keyboard.rb
+++ app/models/keyboard.rb
@@ -4,7 +4,7 @@ class Keyboard < ApplicationRecord
   has_many :keyboard_tags, dependent: :destroy
   has_many :tags, dependent: :destroy, through: :keyboard_tags
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
   validates :price, presence: true
   validates :os, presence: true
   validates :caption, presence: true
