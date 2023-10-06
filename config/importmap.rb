962302c80731f486369b190d2f49571cac8a4e89
ke-sato <keta0616@icloud.com>
2023-10-06 20:46:44 +0900

    after_rubocop_a

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,20 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
-<<<<<<< HEAD
-pin 'application', preload: true
-pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
-pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
-pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
-pin 'flowbite', to: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.turbo.min.js'
-pin_all_from 'app/javascript/controllers', under: 'controllers'
-
-#  追加したファイル
-pin_all_from 'app/javascript/autocomplete', under: 'autocomplete'
-=======
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.turbo.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
->>>>>>> main
