eaf5227d23c7a339b5f0e132d365087c2f722b17
ke-sato <keta0616@icloud.com>
2023-10-20 10:15:19 +0900

    edit_herder_layout

--- app/javascript/autocomplete.js
+++ app/javascript/autocomplete.js
@@ -13,7 +13,7 @@ document.addEventListener("turbo:load", function() {
             keyboardResults.innerHTML = "";
             data.slice(0, 10).forEach(name => {
               const p = document.createElement("p");
-              p.classList.add("truncate", "border-2", "py-2", "border-slate-400");
+              p.classList.add("truncate", "border-2", "py-2", "border-slate-400", "text-slate-700", "hover:bg-slate-100");
               p.setAttribute("data-text", name);
               p.textContent = name.substring(0, 35);
               p.addEventListener("click", function() {
