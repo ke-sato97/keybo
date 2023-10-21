639408ad1d53300cee075ceae69b315c6af55785
ke-sato <keta0616@icloud.com>
2023-10-21 21:33:57 +0900

    generate_docker_file

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: Rails.application.credentials.gmail[:user_name]
+  default from: Rails.application.credentials.gmail[:user_name]  #Gmailアドレス（credentials.yml.encに記載）
   def reset_password_email(user)
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
