3889d8bb4fc6f4212df36b68181ba90913269e2a
ke-sato <keta0616@icloud.com>
2023-10-23 09:19:12 +0900

    apply_password_reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
