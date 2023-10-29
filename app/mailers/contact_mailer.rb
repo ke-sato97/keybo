18a57bcb9bb9c84916281839e92addbe353f9bc8
ke-sato <keta0616@icloud.com>
2023-10-29 12:28:00 +0900

    edit_image_layout

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,5 @@ class ContactMailer < ApplicationMailer
   def send_mail(contact)
     @contact = contact
     mail to: 'ke.sato97@gmail.com', subject: '【お問い合わせ】' + @contact.subject
-    # mail to: ENV['MAIL_ADDRESS'], subject: '【お問い合わせ】' + @contact.subject
   end
 end
