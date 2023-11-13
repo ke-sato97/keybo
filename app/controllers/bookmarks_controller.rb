bb2aa1ed48ad7380636f8250adf855d318359ce3
ke-sato <keta0616@icloud.com>
2023-11-13 16:13:11 +0900

    edit_link_button_layout

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,14 +9,14 @@ class BookmarksController < ApplicationController
   def create
     @keyboard = Keyboard.find(params[:keyboard_id])
     current_user.bookmark(@keyboard)
-    redirect_to keyboards_path
+    redirect_to keyboard_path(@keyboard)
     flash[:success] = 'お気に入り登録しました'
   end
 
   def destroy
     @keyboard = current_user.bookmarks.find(params[:id]).keyboard
     current_user.unbookmark(@keyboard)
-    redirect_to keyboards_path
+    redirect_to keyboard_path(@keyboard)
     # redirect_back fallback_location: keyboards_path
     flash[:success] = 'お気に入りを解除しました'
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,8 +73,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: ENV['MAIL_ADDRESS']
-    password: ENV['SMTP_PASSWORD']
+    user_name: ENV['MAIL_ADDRESS'],
+    password: ENV['SMTP_PASSWORD'],
     authentication: :login
   }
 end
