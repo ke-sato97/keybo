f61081011b7d00f8e80578b432dfca48caaece3f
ke-sato <keta0616@icloud.com>
2023-11-23 22:54:57 +0900

    rspec_system_spec_contact

--- spec/system/contacts_spec.rb
+++ spec/system/contacts_spec.rb
@@ -2,19 +2,46 @@ require 'rails_helper'
 
 RSpec.describe 'Contacts', type: :system do
   let(:user) { create(:user) }
-  let(:other_user) { create(:user) }
+  let(:contact) { create(:contact) }
 
   describe 'contacts' do
     describe 'お問い合わせフォーム' do
       before { login(user) }
 
       context 'フォームの入力が正常' do
-        it '1.コメント成功' do
+        it '2.お問い合わせ確認画面' do
+          visit new_contact_path
+          fill_in '名前', with: 'test'
+          fill_in 'メールアドレス', with: 'test@email.com'
+          fill_in '件名', with: 'test'
+          fill_in 'メッセージ', with: 'test'
+          click_button '入力内容確認'
+
+          # ここで確認画面に遷移していることを確認
+          expect(current_path).to eq contacts_confirm_path
+
+          # ここで期待されるデータを表示しているかを確認
+          expect(page).to have_content 'test'
+          expect(page).to have_content 'test@email.com'
+          expect(page).to have_content 'test'
+          expect(page).to have_content 'test'
+
+          # フォームの送信
+          click_button '送信'
+          expect(current_path).to eq contacts_done_path
         end
       end
 
       context 'フォーム未記入' do
-        it '2.コメント失敗' do
+        it '2.お問い合わせ失敗' do
+          visit new_contact_path
+          fill_in '名前', with: nil
+          fill_in 'メールアドレス', with: 'test@email.com'
+          fill_in '件名', with: 'test'
+          fill_in 'メッセージ', with: 'test'
+          click_button '入力内容確認'
+          expect(current_path).to eq contacts_confirm_path
+          expect(page).to have_content "を入力してください"
         end
       end
     end
