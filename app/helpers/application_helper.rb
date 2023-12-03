c80b69b82695addc6b0f1d752ea77bdacacf610f
ke-sato <keta0616@icloud.com>
2023-12-03 12:03:52 +0900

    dynamic_title_admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def page_title(page_title = '')
-    base_title = 'キー坊'
+  def page_title(page_title = '', admin = false)
+    base_title = if admin
+                  'キー坊(管理画面)'
+                 else
+                   'キー坊'
+                 end
 
     page_title.empty? ? base_title : page_title
   end
