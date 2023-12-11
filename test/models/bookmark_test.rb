3b605cd1378e47b0a8f8852f8e2dd69a9e9cb59c
ke-sato <keta0616@icloud.com>
2023-12-12 00:06:17 +0900

    delete_test_file

--- test/models/bookmark_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class BookmarkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/keyboard_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class KeyboardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
